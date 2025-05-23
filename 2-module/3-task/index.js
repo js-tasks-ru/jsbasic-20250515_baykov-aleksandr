de6e752ff1c61379b74651ed42173f8b4b04af45
Baykov Aleksandr <baykov-1988@mail.ru>
2025-05-23 22:43:46 +0300

    solution of task 2-module/3-task/index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = +prompt('Число a');
+    this.b = +prompt('Число b');
+  },
+
+  sum(a, b) {
+    return this.a + this.b;
+  },
+
+  mul(a, b) {
+    return this.a * this.b;
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
