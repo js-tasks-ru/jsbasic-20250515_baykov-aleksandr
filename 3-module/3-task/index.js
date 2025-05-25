10193de952fdad830b81178fdcc85532093744bf
Baykov Aleksandr <baykov-1988@mail.ru>
2025-05-25 21:21:50 +0300

    solution of task 3-module/3-task/index.js

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+
+  let result = str.split('-');
+  for (let i = 1; i < result.length; i++) {
+    let firstLetter = result[i];
+    result[i] = firstLetter[0].toUpperCase() + firstLetter.slice(1);
+  }
+  return result.join('');
+  
 }
