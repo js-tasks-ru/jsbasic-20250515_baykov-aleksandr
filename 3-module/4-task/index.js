d17484cddb54c4bcfcb42c9cf8eb3028bab1a6e8
Baykov Aleksandr <baykov-1988@mail.ru>
2025-05-26 19:32:03 +0300

    solution of task 3-module/4-task/index.js

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = [];
+  for (let user of users) {
+    if (user.age <= age) {
+      result.push(`${user.name}, ${user.balance}`);
+    }
+  }
+  return result.join("\n");
 }
