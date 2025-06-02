6a9ade9b0325714e591191cde408a29722a4eace
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-02 20:39:42 +0300

    solution of task 4-module/3-task/index.js

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  const tableRows = table.querySelectorAll("tr");
+
+  for (let tableRow of tableRows) {
+    const ageCell = tableRow.querySelector("td:nth-child(2)");
+    const genderCell = tableRow.querySelector("td:nth-child(3)");
+    const isAvailableCell = tableRow.querySelector("td:nth-child(4)");
+
+    if (!isAvailableCell.hasAttribute("data-available")) {
+      tableRow.setAttribute("hidden", "true");
+    }
+    if (isAvailableCell.dataset.available === "true") {
+      tableRow.classList.add("available");
+    }
+    if (isAvailableCell.dataset.available === "false") {
+      tableRow.classList.add("unavailable");
+    }
+
+    if (genderCell.textContent === "m") {
+      tableRow.classList.add("male");
+    }
+
+    if (genderCell.textContent === "f") {
+      tableRow.classList.add("female");
+    }
+
+    if (Number(ageCell.textContent) < 18) {
+      tableRow.setAttribute("style", "text-decoration: line-through");
+    }
+  }
 }
