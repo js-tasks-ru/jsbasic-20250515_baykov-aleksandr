a5cecbd4fea7582ac4e176cd66d8ce1c85465653
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-06 23:31:08 +0300

    solution of task 5-module-3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,56 +1,35 @@
 function initCarousel() {
-  const carousel__container = document.querySelector('.carousel');
-  const carousel = carousel__container.querySelector('.carousel__inner');
-  const carousel__slide = carousel.querySelector('.carousel__slide');
+  const carousel__container = document.querySelector(".carousel");
+  const carousel = carousel__container.querySelector(".carousel__inner");
+  const carousel__slide = carousel.querySelector(".carousel__slide");
+  const carousel__arrow_right = document.querySelector(
+    ".carousel__arrow_right"
+  );
+  const carousel__arrow_left = document.querySelector(".carousel__arrow_left");
   const carousel__slide_width = carousel__slide.offsetWidth;
 
-  let counter = 0; //счетчик переключения слайдов влево/вправо
+  let counter = 0;
+  carousel__arrow_left.style.display = "none";
 
-  carousel__container.addEventListener('click', (evt) => {
+  carousel__container.addEventListener("click", (evt) => {
     const arrow = evt.target.closest('[class^="carousel"]');
-    
 
-    if (arrow.classList.contains('carousel__arrow_left')) {
-      counter -= 1;
-      carousel.style.transform = `translateX(${carousel__slide_width * counter}px)`;
-
-    }
-
-    if (arrow.classList.contains('carousel__arrow_right')) {
+    if (arrow.classList.contains("carousel__arrow_left") && counter < 0) {
       counter += 1;
-      carousel.style.transform = `translateX(${carousel__slide_width * counter}px)`;
-
+      carousel.style.transform = `translateX(${
+        carousel__slide_width * counter
+      }px)`;
+      console.log(counter);
     }
-  })
+
+    if (arrow.classList.contains("carousel__arrow_right") && counter < 3) {
+      counter -= 1;
+      carousel.style.transform = `translateX(${
+        carousel__slide_width * counter
+      }px)`;
+    }
+
+    carousel__arrow_right.style.display = counter === -3 ? "none" : "";
+    carousel__arrow_left.style.display = counter === 0 ? "none" : "";
+  });
 }
-/*Слайды должны перемещаться влево/вправо при клике по кнопкам вперёд/назад.
-
-Их CSS классы:
-- `.carousel__arrow_right` - класс кнопки переключения на слайд вперёд;
-- `.carousel__arrow_left` - класс кнопки переключения на слайд назад;
-
-Все слайды равны по ширине. В этом задании их для простоты ровно 4, и на это количество можно опираться в коде.
-
-### Как (технически) переключается карусель?
-
-Структура карусели такова, что есть внешний элемент, в котором находится "лента" из подряд идущих слайдов. Внешний элемент имеет фиксированную ширину, поэтому видна только часть ленты (один слайд).
-
-CSS класс элемента-ленты, в котором находятся все слайды - `.carousel__inner`. Для переключения слайда мы будем сдвигать его на ширину одного слайда.
-
-Допустим, что ширина одного слайда `300px`. Она может быть любой, точную ширину элемента, независимо от вёрстки, можно получить при помощи его свойства `offsetWidth`. Там хранится только число, без `px`. [Подробнее про offsetWidth](https://learn.javascript.ru/size-and-scroll#offsetwidth-height).
-
-Чтобы переключить на второй слайд, нужно переместить элемент с классом `.carousel__inner` на `300px` влево. Это можно сделать, изменив его свойство transform следующим образом: `elem.style.transform = 'translateX(-300px)'`. 
-
-Мы используем отрицательное значение пикселей, т.к. нам нужно сдвинуть весь элемент влево, если бы значение было положительное, он переместился бы наоборот вправо. [Подробнее про свойство style](https://learn.javascript.ru/styles-and-classes#element-style).
-
- Чтобы переключить ещё на один слайд вперёд, нужно наш элемент сдвинуть ещё на `300px`, вот так: `elem.style.transform = 'translateX(-600px)` и т.д. 
-
-### Скрываем кнопки переключения при достижении крайних слайдов
-
-Когда пользователь дошёл до 4-ого слайда, нужно скрыть кнопку переключения вперёд, и наоборот, когда пользователь видит первый слайд, нужно скрыть кнопку переключения назад. 
-
-Скрывать и показывать кнопки нужно с помощью CSS свойства `display`, вот так: 
-- `carouselArrow.style.display = 'none'` - скрыть кнопку,
-- `carouselArrow.style.display = ''` - показать кнопку,
-
-(Предполагается, что в переменной `carouselArrow` содержится ссылка на кнопку переключения слайдов). */
