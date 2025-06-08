2f7034de2da9da4e1a88cbe1401aca6d0e5cbcde
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-08 14:21:52 +0300

    solution of task 6-module/1-task/index.js

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,52 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this._rows = rows;
+    this._elem = this._createTable();
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  _createTable() {
+    const table = document.createElement("table");
+
+    const thead = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+
+    ["Имя", "Возраст", "Зарплата", "Город", ""].forEach((text) => {
+      const th = document.createElement("th");
+      th.textContent = text;
+      headerRow.appendChild(th);
+    });
+
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    const tbody = document.createElement("tbody");
+
+    this._rows.forEach((row) => {
+      const tr = document.createElement("tr");
+
+      [...Object.keys(this._rows[1])].forEach((key) => {
+        const td = document.createElement("td");
+        td.textContent = row[key];
+        tr.appendChild(td);
+      });
+
+      const deleteTd = document.createElement("td");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "X";
+      deleteBtn.addEventListener("click", () => {
+        tr.remove();
+      });
+
+      deleteTd.appendChild(deleteBtn);
+      tr.appendChild(deleteTd);
+      tbody.appendChild(tr);
+    });
+
+    table.appendChild(tbody);
+    return table;
   }
 }
