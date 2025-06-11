a26b0c642ce431240ade6131f790a1f10af53f0b
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-11 19:49:56 +0300

    fixed offsetWidth

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -25,12 +25,15 @@ export default class Carousel {
       </div>
     `);
 
-    const inner = carousel.querySelector('.carousel__inner');
-    
-    this.slides.forEach(slide => {
-      inner.append(createElement(`
+    const inner = carousel.querySelector(".carousel__inner");
+
+    this.slides.forEach((slide) => {
+      inner.append(
+        createElement(`
         <div class="carousel__slide" data-id="${slide.id}">
-          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="${slide.name}">
+          <img src="/assets/images/carousel/${
+            slide.image
+          }" class="carousel__img" alt="${slide.name}">
           <div class="carousel__caption">
             <span class="carousel__price">€${slide.price.toFixed(2)}</span>
             <div class="carousel__title">${slide.name}</div>
@@ -39,41 +42,41 @@ export default class Carousel {
             </button>
           </div>
         </div>
-      `));
+      `)
+      );
     });
 
     return carousel;
   }
 
   #initCarousel() {
-    const carouselInner = this._elem.querySelector('.carousel__inner');
-    
-    
+    const carouselInner = this._elem.querySelector(".carousel__inner");
+
     setTimeout(() => {
       this.slideWidth = carouselInner.offsetWidth;
       this.#updateCarousel();
     }, 0);
 
-    this._elem.addEventListener('click', (event) => {
-      
-      if (event.target.closest('.carousel__button')) {
-        const slide = event.target.closest('.carousel__slide');
-        this._elem.dispatchEvent(new CustomEvent('product-add', {
-          detail: slide.dataset.id,
-          bubbles: true
-        }));
+    this._elem.addEventListener("click", (event) => {
+      if (event.target.closest(".carousel__button")) {
+        const slide = event.target.closest(".carousel__slide");
+        this._elem.dispatchEvent(
+          new CustomEvent("product-add", {
+            detail: slide.dataset.id,
+            bubbles: true,
+          })
+        );
         return;
       }
 
-      
-      const arrow = event.target.closest('.carousel__arrow');
+      const arrow = event.target.closest(".carousel__arrow");
       if (!arrow) return;
 
-      if (arrow.classList.contains('carousel__arrow_right')) {
+      if (arrow.classList.contains("carousel__arrow_right")) {
         if (this.currentSlideIndex < this.slides.length - 1) {
           this.currentSlideIndex++;
         }
-      } else if (arrow.classList.contains('carousel__arrow_left')) {
+      } else if (arrow.classList.contains("carousel__arrow_left")) {
         if (this.currentSlideIndex > 0) {
           this.currentSlideIndex--;
         }
@@ -84,15 +87,15 @@ export default class Carousel {
   }
 
   #updateCarousel() {
-    const carouselInner = this._elem.querySelector('.carousel__inner');
-    const arrowRight = this._elem.querySelector('.carousel__arrow_right');
-    const arrowLeft = this._elem.querySelector('.carousel__arrow_left');
+    const carouselInner = this._elem.querySelector(".carousel__inner");
+    const arrowRight = this._elem.querySelector(".carousel__arrow_right");
+    const arrowLeft = this._elem.querySelector(".carousel__arrow_left");
+    carouselInner.style.transform = `translateX(-${
+      this.currentSlideIndex * this._elem.offsetWidth
+    }px)`;
 
-    
-    carouselInner.style.transform = `translateX(-${this.currentSlideIndex * 500}px)`;
-    
-    
-    arrowLeft.style.display = this.currentSlideIndex === 0 ? 'none' : '';
-    arrowRight.style.display = this.currentSlideIndex === this.slides.length - 1 ? 'none' : '';
+    arrowLeft.style.display = this.currentSlideIndex === 0 ? "none" : "";
+    arrowRight.style.display =
+      this.currentSlideIndex === this.slides.length - 1 ? "none" : "";
   }
 }
