0e44e06bbe483de1031429d296a481fbf7814104
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-18 00:10:10 +0300

    modified:   8-module/1-task/index.js

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,10 +1,12 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
     this.render();
 
     this.addEventListeners();
+    this.initialTopCoord =
+      this.elem.getBoundingClientRect().top + window.pageYOffset;
   }
 
   render() {
@@ -13,54 +15,71 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    const initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    const leftIndent =
+      Math.min(
+        document.querySelector(".container").getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + "px";
 
-    if (window.pageYOffset > initialTopCoord) {
-      // плавающая корзина
-      this.elem.style.position = 'fixed';
-      this.elem.style.top = 50 + 'px';
-      this.elem.style.right = 10 + 'px';
+      let isMobile = document.documentElement.clientWidth <= 767;
+
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+      return;
+    } 
+
+    if (window.pageYOffset > this.initialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        top: 50 + "px",
+        right: 10 + "px",
+        left: leftIndent,
+        zIndex: 1000,
+      });
     } else {
-      // корзина сверху
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        right: "",
+        zIndex: "",
+      });
     }
-
-    let isMobile = document.documentElement.clientWidth <= 767;
-
-// Если условие выполняется, обнуляем стили к исходным
-if (document.documentElement.clientWidth <= 767) {
-  Object.assign(this.elem.style, {
-    position: '',
-    top: '',
-    left: '',
-    zIndex: ''
-  });
-}
-
   }
 }
