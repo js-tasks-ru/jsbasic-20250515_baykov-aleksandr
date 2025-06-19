3617ef1d53f6804081a6472241e2a932e773981a
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-19 22:26:03 +0300

    implemented card list rendering

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,23 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`<div class="products-grid">
+      <div class="products-grid__inner">
+      </div>
+    </div>`);
+
+    const productInner = this.elem.querySelector(".products-grid__inner");
+    this.products.forEach((item) => {
+      const card = new ProductCard(item);
+      productInner.append(card.elem);
+    });
   }
 }
