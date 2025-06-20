530fd378b7b2bd10ce5afc231d855c7fdbd9d194
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-20 22:39:18 +0300

    add addProduct() method

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,7 +6,42 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;
+    }
+
+    let cartItem = this.cartItems.find(
+      (item) => item.product.id === product.id
+    );
+
+    if (!cartItem) {
+      cartItem = {
+        product,
+        count: 1,
+      };
+
+      this.cartItems.push(cartItem);
+    } else {
+      cartItem.count += 1;
+    }
+
+    /*В качестве аргумента принимает объект товара вида:
+
+let product = {
+  name: "Laab kai chicken salad", // название товара
+  price: 10, // цена товара
+  category: "salads",
+  image: "laab_kai_chicken_salad.png",
+  id: "laab-kai-chicken-salad"
+};
+
+cart.addProduct(product);
+Требования к реализации метода:
+
+Если товара еще нет в корзине, то добавить его в массив cartItems с количеством 1:
+Если товар уже есть в корзине, то увеличить его количество на единицу.
+С обновлённым элементом cartItem вызвать метод onProductUpdate:*/
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
@@ -31,4 +66,3 @@ export default class Cart {
     this.cartIcon.update(this);
   }
 }
-
