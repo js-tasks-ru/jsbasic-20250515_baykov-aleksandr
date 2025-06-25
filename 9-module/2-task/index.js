844a3d319159f5eb2090c93b85f2f2125fe5a789
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-25 21:05:58 +0300

    add fetch

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -1,29 +1,51 @@
-import Carousel from '../../6-module/3-task/index.js';
-import slides from '../../6-module/3-task/slides.js';
+import Carousel from "../../6-module/3-task/index.js";
+import slides from "../../6-module/3-task/slides.js";
 
-import RibbonMenu from '../../7-module/1-task/index.js';
-import categories from '../../7-module/1-task/categories.js';
+import RibbonMenu from "../../7-module/1-task/index.js";
+import categories from "../../7-module/1-task/categories.js";
 
-import StepSlider from '../../7-module/4-task/index.js';
-import ProductsGrid from '../../8-module/2-task/index.js';
+import StepSlider from "../../7-module/4-task/index.js";
+import ProductsGrid from "../../8-module/2-task/index.js";
 
-import CartIcon from '../../8-module/1-task/index.js';
-import Cart from '../../8-module/4-task/index.js';
+import CartIcon from "../../8-module/1-task/index.js";
+import Cart from "../../8-module/4-task/index.js";
 
 export default class Main {
-
-  constructor() {
-  }
+  constructor() {}
 
   async render() {
-    document.querySelector('div[data-carousel-holder]').append(new Carousel(slides)._elem);
-    document.querySelector('div[data-ribbon-holder]').append(new RibbonMenu(categories)._elem);
-    document.querySelector('div[data-slider-holder]').append(new StepSlider({steps: 5, value: 3})._elem);
+    document
+      .querySelector("div[data-carousel-holder]")
+      .append(new Carousel(slides)._elem);
+    document
+      .querySelector("div[data-ribbon-holder]")
+      .append(new RibbonMenu(categories)._elem);
+    document
+      .querySelector("div[data-slider-holder]")
+      .append(new StepSlider({ steps: 5, value: 3 })._elem);
     const cartIcon = new CartIcon();
-    document.querySelector('div[data-cart-icon-holder]').append(cartIcon.elem);
+    document.querySelector("div[data-cart-icon-holder]").append(cartIcon.elem);
     const cart = new Cart(cartIcon);
 
+    async function productGridRender() {
+      try {
+        let response = await fetch("products.json");
 
-    //Создайте экземпляр компонента Cart передав ему как аргумент созданный ранее экземпляр компонента CartIcon.
+        if (!response.ok) {
+          throw new Error("Нет данных");
+        }
+
+        let data = await response.json();
+
+        let productGrid = document.querySelector(
+          "div[data-products-grid-holder]"
+        );
+        productGrid.innerHTML = "";
+        productGrid.append(new ProductsGrid(data).elem);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+    productGridRender();
   }
 }
