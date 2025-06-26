c8ceda793c5711215ef73b00890f41aeb80c604e
Baykov Aleksandr <baykov-1988@mail.ru>
2025-06-26 11:36:36 +0300

    add update filter

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -17,35 +17,46 @@ export default class Main {
     document
       .querySelector("div[data-carousel-holder]")
       .append(new Carousel(slides)._elem);
+    this.ribbonMenu = new RibbonMenu(categories);
     document
       .querySelector("div[data-ribbon-holder]")
-      .append(new RibbonMenu(categories)._elem);
+      .append(this.ribbonMenu._elem);
+    this.stepSlider = new StepSlider({ steps: 5, value: 3 });
     document
       .querySelector("div[data-slider-holder]")
-      .append(new StepSlider({ steps: 5, value: 3 })._elem);
+      .append(this.stepSlider._elem);
     const cartIcon = new CartIcon();
     document.querySelector("div[data-cart-icon-holder]").append(cartIcon.elem);
     const cart = new Cart(cartIcon);
 
-    async function productGridRender() {
-      try {
-        let response = await fetch("products.json");
+    await this.productGridRender();
 
-        if (!response.ok) {
-          throw new Error("Нет данных");
-        }
+    this.productsGrid.updateFilter({
+      noNuts: document.getElementById("nuts-checkbox").checked,
+      vegeterianOnly: document.getElementById("vegeterian-checkbox").checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value,
+    });
+  }
 
-        let data = await response.json();
+  async productGridRender() {
+    try {
+      let response = await fetch("products.json");
 
-        let productGrid = document.querySelector(
-          "div[data-products-grid-holder]"
-        );
-        productGrid.innerHTML = "";
-        productGrid.append(new ProductsGrid(data).elem);
-      } catch (e) {
-        console.log(e.message);
+      if (!response.ok) {
+        throw new Error("Нет данных");
       }
+
+      let data = await response.json();
+
+      let productGrid = document.querySelector(
+        "div[data-products-grid-holder]"
+      );
+      productGrid.innerHTML = "";
+      this.productsGrid = new ProductsGrid(data);
+      productGrid.append(this.productsGrid.elem);
+    } catch (e) {
+      console.log(e.message);
     }
-    productGridRender();
   }
 }
